c47e74307f3ed5c7200b9d389ea001abc928c867
Michail Zyusko <michail.zyusko@gmail.com>
2022-08-20 17:23:27 +0300

    Update get-app-version.sh

--- .github/workflows/scripts/get-app-version.sh
+++ .github/workflows/scripts/get-app-version.sh
@@ -1,7 +1,8 @@
 readarray -t arr <<< "v1.0.0 /n COMMIT_MESSAGE"
 
-echo ${arr[0]}
-echo ${arr[1]}
+echo "${arr[0]}"
+echo "123123123"
+echo "${arr[1]}"
 
 export APP_VERSION=${arr[0]}
 export COMMIT_MESSAGE=${arr[1]}
