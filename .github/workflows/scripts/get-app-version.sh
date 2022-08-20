1a02bb1c11ce1c85058a28db79a760b3212d4fdd
Michail Zyusko <michail.zyusko@gmail.com>
2022-08-20 17:30:58 +0300

    Fix version issues

--- .github/workflows/scripts/get-app-version.sh
+++ .github/workflows/scripts/get-app-version.sh
@@ -1,11 +1,7 @@
-text="this is line one
-this is line two
-this is line three"
-readarray -t arr <<< "$text"
+readarray -t arr <<< "$github.event.head_commit.message"
 
 echo "${arr[0]}"
-echo "123123123"
 echo "${arr[1]}"
 
-export APP_VERSION=${arr[0]}
-export COMMIT_MESSAGE=${arr[1]}
+export APP_VERSION=${arr[0] || $github.ref}
+export COMMIT_MESSAGE=${arr[1] || $github.event.head_commit.message}
